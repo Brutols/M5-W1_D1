37547f08d8704b98f720e271b943cd5db7fc5143
Brutols <dtwo97@gmail.com>
2024-02-13 13:45:04 +0100

    working search, card selection, axios error handling

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,16 +1,24 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import styles from "./card.module.css";
+import { useState } from "react";
 
 function CardElement(props) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleCardClick = (e) => {
+    setIsSelected(!isSelected);
+  };
+
   return (
-    <Card className={styles.card_element}>
+    <Card
+      className={!isSelected ? styles.card_element : styles.border_red}
+      onClick={handleCardClick}
+    >
       <Card.Img variant="top" src={props.src} className={styles.card_img} />
       <Card.Body>
-        <Card.Title className='text-truncate'>{props.title}</Card.Title>
-        <Card.Text>
-          {props.desc}
-        </Card.Text>
+        <Card.Title className="text-truncate">{props.title}</Card.Title>
+        <Card.Text>{props.desc}</Card.Text>
         <Button variant="primary">{props.price}</Button>
       </Card.Body>
     </Card>
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -9,14 +9,32 @@ import CardElement from "../Card/Card";
 const Main = () => {
   const [loading, setLoading] = useState(false);
   const [books, setBooks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [filteredBooks, setFilteredBooks] = useState([]);
 
   useEffect(() => {
     const getBooks = async () => {
       setLoading(true);
 
-      const res = await axios.get(
-        "https://striveschool-api.herokuapp.com/books"
-      );
+      const res = await axios
+        .get("https://striveschool-api.herokuapp.com/books")
+        .catch(function (error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser
+            // and an instance of http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+        });
 
       setBooks(res.data);
 
@@ -25,22 +43,55 @@ const Main = () => {
     getBooks();
   }, []);
 
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+
+    inputValue.length <= 1
+      ? setFilteredBooks([])
+      : setFilteredBooks(
+          books.filter((book) =>
+            book.title.toLowerCase().includes(inputValue.toLowerCase())
+          )
+        );
+  };
+
   return (
-    <Container className={classes.card_wrapper}>
-      {loading ? (
-        <MyAlert variant="danger" text="Loading..." />
-      ) : (
-        books.map((book) => (
-          <CardElement
-            key={nanoid()}
-            title={book.title}
-            desc={book.category}
-            price={book.price}
-            src={book.img}
-          />
-        ))
-      )}
-    </Container>
+    <>
+      <input
+        type="text"
+        className="form-control mt-4 w-50 mx-auto"
+        placeholder="Search"
+        aria-label="Search"
+        aria-describedby="basic-addon1"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <Container className={classes.card_wrapper}>
+        {loading ? (
+          <MyAlert variant="danger" text="Loading..." />
+        ) : filteredBooks.length > 0 ? (
+          filteredBooks.map((book) => (
+            <CardElement
+              key={nanoid()}
+              title={book.title}
+              desc={book.category}
+              price={book.price}
+              src={book.img}
+            />
+          ))
+        ) : (
+          books.map((book) => (
+            <CardElement
+              key={nanoid()}
+              title={book.title}
+              desc={book.category}
+              price={book.price}
+              src={book.img}
+            />
+          ))
+        )}
+      </Container>
+    </>
   );
 };
 
